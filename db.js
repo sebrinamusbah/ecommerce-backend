ffee012d32eb9a82b880a35dd5f3a201f48ea540
sebrinamusbah <sebrinm9@gmail.com>
2025-12-18 18:07:48 +0300

    update db.js

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required on Render
 });
 
 module.exports = pool;
